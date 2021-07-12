b4071a91c822cda69a9f410d417a8e8bde15242e
97tuna <ldh2033@tunahouse97.com>
2021-07-13 01:23:38 +0900

    [ADD] Firebase

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import Kingfisher
 import AVFoundation
+import Firebase
 
 class SearchViewController: UIViewController {
 
+    let db = Database.database().reference().child("searchHistory")
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultCollectionView: UICollectionView!
@@ -113,6 +115,8 @@ extension SearchViewController: UISearchBarDelegate {
             DispatchQueue.main.async {
                 self.movies = movies
                 self.resultCollectionView.reloadData()
+                let timestamp = Date().timeIntervalSince1970.rounded()
+                self.db.childByAutoId().setValue(["term": searchTerm, "timestamp": timestamp])
             }
         }
     }
